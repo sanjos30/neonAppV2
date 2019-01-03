47ce3dd07b65933d18fb7091d7b5958ecced418a
SanjoS30 <sandeep.combat@gmail.com>
2019-01-03 20:12:25 +1100

    minor bug fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,7 +27,7 @@ export class MyApp {
 
 
     firebase.initializeApp({
-      apiKey: "",
+      apiKey: "AIzaSyC4b_qyg3xGPWXEdH4B-VLCRu1ClbjNBwk",
       authDomain: "neonappservertest.firebaseapp.com",
       databaseURL: "https://neonappservertest.firebaseio.com",
     });
